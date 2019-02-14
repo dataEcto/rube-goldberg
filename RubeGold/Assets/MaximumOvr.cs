be9c55f9b76f4b8ed894f6e9efa2404c55560cf5
Genric Jan Elazegui <gj.elazegui@Gmail.com>
2019-02-14 12:40:22 -0500

    Part 1

---
+++ RubeGold/Assets/MaximumOvr.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaximumOvr : MonoBehaviour
+{
+    public float thrust;
+    Rigidbody my_rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+       my_rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        my_rb.AddForce(transform.forward * thrust);
+    }
+}
