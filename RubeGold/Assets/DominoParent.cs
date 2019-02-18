f4b1ff27e4f5a5dd90837e906fd3c935b43a2baa
Genric <gj.elazegui@Gmail.com>
2019-02-17 19:45:49 -0500

    Homestretch
    Its almost done, actually. The only problematic part is that the ghost dominoes act very weird...but i dont have to worry too much about how the dominoes start falling. just that its all lined up right.

---
+++ RubeGold/Assets/DominoParent.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DominoParent : MonoBehaviour
+{
+    private Transform _dominoParent;
+    private GameObject _dominoOne;
+    private bool dropDominoes = false;
+    
+
+    void Start()
+    {
+        _dominoParent = GameObject.Find("Ghost").GetComponent<Transform>();
+        _dominoOne = GameObject.FindGameObjectWithTag("GameController");
+        //Go through the child of the object (In this case, the dominoes
+        //and set their rigidbody kinematic components to true
+        foreach (Transform child in _dominoParent)
+        {
+            if (child.GetComponent<Rigidbody>())
+            {
+                child.GetComponent<Rigidbody>().isKinematic = true;
+            }
+        }
+        
+     
+    }
+
+    private void Update()
+    {
+        if (_dominoOne.GetComponent<TriggerCheck>().isTriggered)
+        {
+            DropDominoes();
+        }
+           
+
+     
+    }
+
+
+
+    void DropDominoes()
+    {
+        
+        foreach (Transform child in _dominoParent)
+        {
+            if (child.GetComponent<Rigidbody>())
+            {
+                child.GetComponent<Rigidbody>().isKinematic = false;
+                child.GetComponent<Rigidbody>().AddForce(Vector3.down);
+            }
+        }
+        
+       
+    }
+}
---
+++ RubeGold/Assets/TriggerCheck.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerCheck : MonoBehaviour
+{
+    public bool isTriggered;
+    public void OnTriggerEnter(Collider other)
+    {
+
+         if (other.tag == "Player")
+            isTriggered = true;
+    }
+}
