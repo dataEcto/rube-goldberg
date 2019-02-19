f2bd2b1ba9121333206f14820d18e3368e0dfb69
Genric Jan Elazegui <gj.elazegui@Gmail.com>
2019-02-18 19:16:37 -0500

    Build DOne

---
+++ RubeGold/Assets/StartingFunction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartingFunction : MonoBehaviour
+{
+    private Rigidbody _myRb;
+
+    private AudioSource _myAs;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _myRb = GetComponent<Rigidbody>();
+        _myAs = GetComponent<AudioSource>();
+
+        _myRb.isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _myRb.isKinematic = false;
+            _myAs.Play();
+        }
+    }
+}
